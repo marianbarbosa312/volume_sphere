fbfc46e45fff48c63606939c5eeeb38ad9d768a1
marianbarbosa312 <70342506+marianbarbosa312@users.noreply.github.com>
2020-09-09 13:59:31 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    Volume_Sphere = 4 / 3 * (Pi * Radius ** 3)
+    basic.showNumber(Volume_Sphere)
+})
+let Volume_Sphere = 0
+let Pi = 0
+let Radius = 0
+Radius = 12
+Pi = 3.146
